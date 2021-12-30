7ac7126111f120049548215f2778e51f931331ab
Ivan <ivsound93@gmail.com>
2021-12-30 14:49:46 +0300

    AdminDishControllerTest

--- src/test/java/ru/graduation/controller/AdminDishControllerTest.java
+++ src/test/java/ru/graduation/controller/AdminDishControllerTest.java
@@ -1,6 +1,7 @@
 package ru.graduation.controller;
 
 
+import org.hibernate.boot.model.naming.DatabaseIdentifier;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -13,9 +14,14 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.filter.CharacterEncodingFilter;
 import ru.graduation.TestUtil;
+import ru.graduation.model.Dish;
+import ru.graduation.service.DishService;
+import ru.graduation.web.exeption.NotFoundException;
+import ru.graduation.web.json.JsonUtil;
 
 import java.util.Locale;
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -37,6 +43,7 @@ public class AdminDishControllerTest {
         CHARACTER_ENCODING_FILTER.setForceEncoding(true);
     }
 
+
     @Autowired
     private MockMvc mockMvc;
 
@@ -44,16 +51,84 @@ public class AdminDishControllerTest {
         return mockMvc.perform(builder);
     }
 
+    private static final String REST_URL = "/" + ADMIN_DISH_REST_URL + "/";
+
+    @Autowired
+    private DishService dishService;
+
     @Test
-    void getByMenuId() throws Exception {
-        perform(MockMvcRequestBuilders.get("/"+ ADMIN_DISH_REST_URL + "/" + DISH1_ID)
-//                .param("menuId", "1")
-                .with(TestUtil.userHttpBasic(admin))
-        )
+    void get() throws Exception {
+        perform(MockMvcRequestBuilders.get(REST_URL + DISH1_ID)
+                .with(TestUtil.userHttpBasic(admin)))
                 .andExpect(status().isOk())
                 .andDo(print())
                 .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
                 .andExpect(DISH_MATCHER.contentJson(dish1));
     }
 
+    @Test
+    void getByMenuId() throws Exception {
+        perform(MockMvcRequestBuilders.get(REST_URL)
+                .param("menuId", "1")
+                .with(TestUtil.userHttpBasic(admin)))
+                .andExpect(status().isOk())
+                .andDo(print())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andExpect(DISH_MATCHER.contentJson(dishes));
+    }
+
+    @Test
+    void getUnauth() throws Exception {
+        perform(MockMvcRequestBuilders.get(REST_URL))
+                .andExpect(status().isUnauthorized());
+    }
+
+    @Test
+    void delete() throws Exception {
+        perform(MockMvcRequestBuilders.delete(REST_URL + DISH1_ID)
+                .param("menuId", "1")
+                .with(TestUtil.userHttpBasic(admin)))
+                .andDo(print())
+                .andExpect(status().isNoContent());
+        assertThrows(NotFoundException.class, () -> dishService.get(DISH1_ID));
+    }
+
+    @Test
+    void deleteNotFound() throws Exception {
+        perform(MockMvcRequestBuilders.delete(REST_URL + NOT_FOUND_DISH)
+                .param("menuId", "1")
+                .with(TestUtil.userHttpBasic(admin)))
+//                .andDo(print())
+                .andExpect(status().isUnprocessableEntity());
+    }
+
+    @Test
+    void update() throws Exception {
+        Dish updated = getUpdated();
+        perform(MockMvcRequestBuilders.put(REST_URL)
+                .param("menuId", "1")
+                .with(TestUtil.userHttpBasic(admin))
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(JsonUtil.writeValue(updated)))
+                .andDo(print())
+                .andExpect(status().isNoContent());
+        DISH_MATCHER.assertMatch(dishService.get(DISH1_ID), getUpdated());
+    }
+
+    @Test
+    void create() throws Exception {
+        Dish newDish = getNew();
+        ResultActions action = perform(MockMvcRequestBuilders.post(REST_URL)
+                .param("menuId", "1")
+                .with(TestUtil.userHttpBasic(admin))
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(JsonUtil.writeValue(newDish)))
+                .andDo(print())
+                .andExpect(status().isCreated());
+
+        Dish created = DISH_MATCHER.readFromJson(action);
+        int newId = created.id();
+        newDish.setId(newId);
+        DISH_MATCHER.assertMatch(dishService.get(newId), newDish);
+    }
 }
